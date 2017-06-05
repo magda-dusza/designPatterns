b68b40fc9ed94f5847ad8028a0e6f7d2020ef672
Magdalena Dusza EX1 <MDusza@pl.imshealth.com>
2017-06-05 11:57:31 +0200

    constructor node approach

--- designPatterns/creational-design-patterns/constructor/task.js
+++ designPatterns/creational-design-patterns/constructor/task.js
@@ -12,12 +12,4 @@ Task.prototype.save = function () {
     console.log('saving Task:', this.name);
 };
 
-var task1 = new Task('create a demo for constructors');
-var task2 = new Task('create a demo for modules');
-var task3 = new Task('create a demo for singlestons');
-var task4 = new Task('create a demo for prototypes');
-
-task1.complete();
-task2.save();
-task3.save();
-task4.save();
+module.exports = Task;
